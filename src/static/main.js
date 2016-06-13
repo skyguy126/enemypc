a1f4cfe9489ff7bddf692a42259a00c89f5186f3
vamsieyunni@gmail.com <vamsieyunni@gmail.com>
2016-06-13 20:51:40 +0000

    cleanup

--- src/static/main.js
+++
@@ -1,9 +0,0 @@
-/* main.js */
-
-$(document).ready(function(){
-    console.log(document.cookie);
-    var socket = io();
-    io.on('connection', function(socket){
-        console.log("kek");
-    });
-});
