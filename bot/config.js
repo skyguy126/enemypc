3c412d96e87a2f339270658ac2edf29a4a129e9b
frostyfox24 <13lenni@gmail.com>
2016-06-12 22:41:00 -0700

    trade bot

---
+++ bot/config.js
@@ -0,0 +1,11 @@
+status = 1;
+steamID = '[U:1:250665304]';
+itemList = ['Indigo','Bamboo'];
+
+var config = {
+ username: "whattheactuals",
+ password: "nignogwelfare69",
+ admin: '76561198210931032'
+};
+
+module.exports = config;
