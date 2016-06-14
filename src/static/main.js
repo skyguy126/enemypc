776d9446c4d699eac60619fe1ba9375901e2ce46
Ishan Bhargava <fireDude90@gmail.com>
2016-06-13 19:38:48 -0700

    exposed socket to global scope

--- src/static/main.js
+++ src/static/main.js
@@ -1,9 +1,7 @@
-/* main.js */
+var socket;
 
 $(function () {
-    console.log("Ready!");
-
-    var socket = io.connect('wss://24.4.237.252/sock');
+    socket = io.connect('wss://24.4.237.252/sock');
     socket.on('connect', function () {
         console.log('connected');
         socket.emit('sock_auth', { "cookie": document.cookie });
