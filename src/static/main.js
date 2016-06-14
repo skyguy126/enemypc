8384083dd5d5f229be2383c274feb91b7b388574
Ishan Bhargava <fireDude90@gmail.com>
2016-06-13 19:16:20 -0700

    sends cookie

--- src/static/main.js
+++ src/static/main.js
@@ -1,30 +1,10 @@
 /* main.js */
 
-$(document).ready(function(){
+$(function () {
     console.log("Ready!");
 
-    //$("#1").click(function(){
-    //    $(this).prop("disabled", true);
-    //    $.ajax({
-    //        type : "POST",
-    //        async : true,
-    //        url : "search",
-    //        data : {
-    //            id : $("#2").val()
-    //        },
-    //        statusCode : {
-    //            200 : function(data) {
-    //                console.log(data);
-    //                $("#1").prop("disabled", false);
-    //            }
-    //        }
-    //    });
-    //});
-
-    var socket = io.connect('/sock');
+    var socket = io.connect('wss://24.4.237.252/sock');
     socket.on('connection', function () {
-        socket.on('data', function (data) {
-            console.log(data);
-        });
+        socket.emit('sock_auth', { "cookie": document.cookie });
     });
 });
