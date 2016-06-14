ef3d1cb9b7ce5d4c12919329a1b98cc0f6096169
Ishan Bhargava <fireDude90@gmail.com>
2016-06-13 19:32:45 -0700

    logging

--- src/static/main.js
+++ src/static/main.js
@@ -5,6 +5,7 @@ $(function () {
 
     var socket = io.connect('wss://24.4.237.252/sock');
     socket.on('connect', function () {
+        console.log('connected');
         socket.emit('sock_auth', { "cookie": document.cookie });
     });
 });
